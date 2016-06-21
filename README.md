efcfd659c7b9ab6347751058eec84376e4106de3
mangelosalvio <msalvio.13@gmail.com>
2016-06-21 22:17:48 +0800

    Added first files

---
+++ README.md
@@ -0,0 +1,1 @@
+Files for CpE50 
---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by msalvio on 21/06/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
