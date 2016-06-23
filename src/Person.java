b3752805826a7a43c224c54292655138525e45db
michaelangelosalvio <m.salvio@usls.edu.ph>
2016-06-23 10:06:54 +0800

    Added Person, TestPerson class

---
+++ src/Person.java
@@ -0,0 +1,39 @@
+public class Person
+{
+    private String firstName;
+    private String lastName;
+    private Person mother;
+    private Person father;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Person getMother() {
+        return mother;
+    }
+
+    public void setMother(Person mother) {
+        this.mother = mother;
+    }
+
+    public Person getFather() {
+        return father;
+    }
+
+    public void setFather(Person father) {
+        this.father = father;
+    }
+}
